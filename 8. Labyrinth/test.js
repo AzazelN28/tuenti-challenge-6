332adb23d05789f5e5c640b28d50e58dbb5d1c27
Aitor Moreno <aitor@rojo2.com>
2016-05-03 13:53:15 +0200

    Added code for Immiscible numbers and removed video.mov

--- 8. Labyrinth/test.js
+++ 8. Labyrinth/test.js
@@ -1,23 +1,215 @@
 "use strict";
 
+const fs = require("fs");
 const net = require("net");
 
+const WIDTH = 84;
+const HEIGHT = 84;
+
 const UP = 'u';
-const DOWN = 'd';
-const LEFT = 'l';
-const RIGHT = 'r';
+const DN = 'd';
+const LT = 'l';
+const RT = 'r';
 
+function createEmptyMap(w,h,c) {
+  const map = [];
+  for (let y = 0; y < h; y++) {
+    const row = [];
+    for (let x = 0; x < w; x++) {
+      row.push(c);
+    }
+    map.push(row);
+  }
+  return map;
+}
 
-const map = new Array(512);
+const map = createEmptyMap(WIDTH, HEIGHT, " ");
+const passes = createEmptyMap(WIDTH, HEIGHT, 0);
+
+let player = {
+  x: WIDTH >> 1,
+  y: HEIGHT >> 1
+};
+
+let lastAction = null;
+let cost = 4096;
+let move = UP;
 
 const client = net.connect(1986, "52.49.91.111");
+
+function moveUp() {
+  lastAction = UP;
+  client.write(UP + "\n");
+}
+
+function moveDown() {
+  lastAction = DN;
+  client.write(DN + "\n");
+}
+
+function moveLeft() {
+  lastAction = LT;
+  client.write(LT + "\n");
+}
+
+function moveRight() {
+  lastAction = RT;
+  client.write(RT + "\n");
+}
+
 client.on("connect", () => {
+
   console.log("conectado!");
+
 }).on("data", (data) => {
-  console.log(data.toString());
-  const currentTiles = data.toString().split("\n").filter((line) => line !== "").map((line) => line.split(""));
-  console.log(currentTiles);
+
+  let string = data.toString();
+  if (/^You can't pass through walls, can you\?$/.test(string)) {
+
+    string = string.replace(/^You can't pass through walls, can you\?$/, "");
+
+    lastAction = null;
+
+  } else {
+
+    passes[player.y][player.x]++;
+
+    switch(lastAction) {
+      case UP: player.y--; break;
+      case DN: player.y++; break;
+      case LT: player.x--; break;
+      case RT: player.x++; break;
+    }
+
+    lastAction = null;
+
+  }
+
+  const rows = string.split("\n")
+                     .filter((line) => line !== "")
+                     .map((line) => line.split(""));
+
+  for (let y = 0; y < rows.length; y++) {
+    const columns = rows[y];
+    for (let x = 0; x < columns.length; x++) {
+      const tile = columns[x];
+
+      const tx = player.x + x - 3;
+      const ty = player.y + y - 3;
+
+      if (tile !== "x") {
+        map[ty][tx] = tile;
+      }
+
+      if (tile === "#") {
+        passes[ty][tx] = 2048;
+      }
+    }
+  }
+
+  const up = map[player.y - 1][player.x];
+  const dn = map[player.y + 1][player.x];
+  const lt = map[player.y][player.x - 1];
+  const rt = map[player.y][player.x + 1];
+
+  const pass_up = passes[player.y - 1][player.x];
+  const pass_dn = passes[player.y + 1][player.x];
+  const pass_lt = passes[player.y][player.x - 1];
+  const pass_rt = passes[player.y][player.x + 1];
+
+  cost = 4096;
+
+  switch(move) {
+    case UP:
+      if (up === '#') { move = null; }
+      break;
+
+    case DN:
+      if (dn === '#') { move = null; }
+      break;
+
+    case LT:
+      if (lt === '#') { move = null; }
+      break;
+
+    case RT:
+      if (rt === '#') { move = null; }
+      break;
+  }
+
+  if (move === RT || move === LT || move === null) {
+
+    if (up !== '#') {
+      if (pass_up <= pass_dn
+       && pass_up <= pass_lt
+       && pass_up <= pass_rt) {
+        cost = pass_up;
+        move = UP;
+        //moveUp();
+      }
+    }
+
+    if (dn !== '#') {
+      if (pass_dn <= pass_up
+       && pass_dn <= pass_lt
+       && pass_dn <= pass_rt) {
+        if (pass_dn < cost) {
+          cost = pass_dn;
+          move = DN;
+        }
+        //moveDown();
+      }
+    }
+
+  }
+
+  if (move === UP || move === DN || move === null) {
+
+    if (rt !== '#') {
+      if (pass_rt <= pass_up
+       && pass_rt <= pass_dn
+       && pass_rt <= pass_lt) {
+        if (pass_rt < cost) {
+          cost = pass_rt;
+          move = RT;
+        }
+        //moveRight();
+      }
+    }
+
+    if (lt !== '#') {
+      if (pass_lt <= pass_up
+       && pass_lt <= pass_dn
+       && pass_lt <= pass_rt) {
+        if (pass_lt < cost) {
+          cost = pass_lt;
+          move = LT;
+        }
+        //moveLeft();
+      }
+    }
+
+  }
+
+  switch(move) {
+    case UP: moveUp(); break;
+    case DN: moveDown(); break;
+    case RT: moveRight(); break;
+    case LT: moveLeft(); break;
+  }
+
+  process.stdout.write("\x1B[2J\x1B[0,0H");
+  process.stdout.write("↑\t↓\t←\t→\n");
+  process.stdout.write(`${up}\t${dn}\t${lt}\t${rt}\n`);
+  process.stdout.write(`${pass_up}\t${pass_dn}\t${pass_lt}\t${pass_rt}\n`);
+  process.stdout.write(string);
+
+  fs.writeFileSync("map.txt", map.map((row) => row.join("")).join("\n"));
+  //console.log(string);
+
 }).on("end", (end) => {
+
   console.log("desconectado!");
+
 });
 
---
+++ 9. Inmiscible Numbers/sample.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const fs = require("fs");
+
+const input = fs.readFileSync("sampleInput.txt")
+                .toString()
+                .split("\n")
+                .filter((value,index) => value !== "")
+                .filter((value,index) => index >= 1)
+                .map((value) => parseInt(value,10));
+
+function isImmiscible(value) {
+  const string = value.toString();
+
+  let i = 0;
+
+  while (string.charAt(i) === "1") {
+    i++;
+  }
+
+  if (i === string.length) {
+    return true;
+  }
+
+  while (string.charAt(i) === "0") {
+    i++;
+  }
+
+  if (i === string.length) {
+    return true;
+  }
+  return false;
+}
+
+function immiscibleCount(value) {
+  const string = value.toString();
+
+  let i = 0,
+      ones = 0,
+      zeroes = 0;
+
+  while (string.charAt(i) === "1") {
+    i++;
+    ones++;
+  }
+
+  if (i === string.length) {
+    return [ones,zeroes];
+  }
+
+  while (string.charAt(i) === "0") {
+    i++;
+    zeroes++;
+  }
+
+  if (i === string.length) {
+    return [ones,zeroes];
+  }
+  return null;
+}
+
+const output = input.map((value) => {
+  process.stderr.write("\x1B[2J");
+  let multiple = 1;
+  while (!isImmiscible(value * multiple)) {
+    process.stderr.write(`\x1B[0,0H${value} ${multiple}`);
+    multiple++;
+  }
+  return immiscibleCount(value * multiple);
+}).map((value,index) => {
+  return `Case #${index+1}: ${value[0]} ${value[1]}`;
+}).join("\n");
+
+console.log(output);
